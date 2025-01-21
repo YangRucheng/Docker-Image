dc737040ae9cd2983db59e76520f7739c014b8a8
Sakana <admin@yangrucheng.top>
2025-01-21 13:21:14 +0800

    Fix Ubuntu

--- ubuntu/entrypoint.sh
+++ ubuntu/entrypoint.sh
@@ -5,6 +5,9 @@ if [ -n "$ROOT_PASSWORD" ]; then
 fi
 
 mkdir -p /run/sshd \
+&& echo "该镜像只持久化 /home 目录" > /root/README.txt \
 && chmod 600 /root/.ssh \
 && chmod 400 /root/.ssh/authorized_keys \
 && /usr/sbin/sshd -D 
+
+exec "$@"
